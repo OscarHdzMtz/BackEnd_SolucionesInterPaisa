a301f5b2654bb9c6a47218b1409da64cddcf4fdc
Oscar Hernandez Martinez <oskr9922@gmail.com>
2022-10-15 12:57:41 -0500

    Creacion del proyecto, con libreria Tik4Net ya agregado, con metodo GET de ejemplo funcionando

---
+++ BackEnd_SolucionesInterPaisa/Controllers/FichasController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tik4net;
+using tik4net.Objects;
+using tik4net.Objects.Ip.Hotspot;
+
+namespace BackEnd_SolucionesInterPaisa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FichasController : ControllerBase
+    {
+        
+        [HttpGet]
+        public async Task<IActionResult> GetFichas()
+        {
+            using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+            {
+                connection.Open("192.168.1.85", "admin", "");
+                ITikCommand cmd = connection.CreateCommand("/system/identity/print");
+                Console.WriteLine(cmd.ExecuteScalar());
+
+                var listPerfiles = connection.LoadAll<HotspotUser>();
+
+                return Ok(listPerfiles);
+
+            }
+
+        }
+    }
+}
